41ed1593057e85e9a51de68863155ed3ff424e63
hiralal <hiralals221@gmail.com>
2024-03-18 19:55:04 +0530

    Added react-router-dom and Home,HubsNearBy components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { Home } from './components/Home/Home';
+import HubsNearBy from './components/HubsNearBy/HubsNearBy';
+
+export const ROUTES = {
+  HOME: '/',
+  HUBS: '/hubs'
+}
 
 const App = () => {
   const [theme, setTheme] = useState('light');
-
   return (
     <div className={`container ${theme}`}>
-      <Navbar theme={theme} setTheme={setTheme} />
+      <Router>
+        <Navbar theme={theme} setTheme={setTheme} />
+        <Routes>
+          <Route path={ROUTES.HOME} element={<Home/>}/>
+          <Route path={ROUTES.HUBS} element={<HubsNearBy/>}/>
+        </Routes>
+      </Router>
     </div>
   )
 }
