6037ba1cb4e707537aea449454aa06ae63e0264c
hiralal <hiralals221@gmail.com>
2024-03-20 01:33:45 +0530

    Added-new-components and modify the Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import hero from '../../assets/hero.jpg'
+import Button1 from '../../components/Buttons/Btn1'
+import '../Home/Home.css'
 
 export const Home = () => {
   return (
     <div>
-      <h1 className="text-center text-4xl font-bold mt-8">Welcome to the home page!</h1>
+      <section className='home' id='home' >
+        <div className="Home_container">
+          <div className="home_content">
+            <p className="subtitle">pickup</p>
+            <h1 className="title">Connect Globally, Deliver Locally</h1>
+            <p className="text">Your Gateway to Seamless Postal Solutions</p>
+          </div>
+          <Button1  className='btn'/>
+          <div className="img">
+            <img src={hero} alt="hero section" />
+          </div>
+        </div>
+
+      </section>
     </div>
   )
 }
+
+
+//anylised the code and wriite css to design this?
