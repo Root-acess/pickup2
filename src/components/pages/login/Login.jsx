4138f5b0e1d1b84ebb0515afbf6d6df0b12d5256
hiralal <hiralals221@gmail.com>
2024-04-06 14:57:50 +0530

    add login and add some components

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -1,40 +1,52 @@
-import React from 'react'
-import '../login/login.css'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import LoginEffect from '../../Effect/LoginEffect';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const handleForgotPassword = () => {
+    return navigate('/forgot-password');
+  };
+
+  const handleRegister = () => {
+    return navigate('/signup');
+  };
+
   return (
-    
-  <div id="form-ui">
-  <form action="" method="post" id="form">
-    <div id="form-body">
-      <div id="welcome-lines">
-        <div id="welcome-line-1">LogIn</div>
-        <div id="welcome-line-2">Welcome Back Sir</div>
-      </div>
-      <div id="input-area">
-        <div class="form-inp">
-          <input placeholder="Email Address" type="text" />
-        </div>
-        <div class="form-inp">
-          <input placeholder="Password" type="password" />
-        </div>
-      </div>
-      <div id="submit-button-cvr">
-        <button id="submit-button" type="submit">Login</button>
-      </div>
-      <div id="forgot-pass">
-        <a href="">Forgot password?</a>
-      </div>
-      <div id="Resister">
-            <a href='../resister/Resiste'>Register Now</a>
+    <>  
+    <LoginEffect />
+    <div id="form-ui">
+      <form action="" method="post" id="form">
+        <div id="form-body">
+          <div id="welcome-lines">
+            <div id="welcome-line-1">LogIn</div>
+            <div id="welcome-line-2">Welcome Back Sir</div>
           </div>
-      <div id="bar"></div>
+          <div id="input-area">
+            <div className="form-inp">
+              <input placeholder="Email Address" type="text" />
+            </div>
+            <div className="form-inp">
+              <input placeholder="Password" type="password" />
+            </div>
+          </div>
+          <div id="submit-button-cvr">
+            <button id="submit-button" type="submit">Login</button>
+          </div>
+          <div id="forgot-pass">
+            <button onClick={handleForgotPassword}>Forgot password?</button>
+          </div>
+          <div id="Resister">
+            <button onClick={handleRegister}>Register Now</button>
+          </div>
+          <div id="bar"></div>
+        </div>
+      </form>
     </div>
-  </form>
-  </div>
+    </>
+    
+  );
+};
 
-
-  )
-}
-
-export default Login
+export default Login;
